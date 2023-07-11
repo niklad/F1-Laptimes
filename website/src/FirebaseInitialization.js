39b5dc897b202509a7fc9bd5d7417a36bab77168
Niklas <niklast@stud.ntnu.no>
2023-07-11 15:21:47 +0200

    Firebase stuff

---
+++ website/src/FirebaseInitialization.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://f1-laptimes-default-rtdb.europe-west1.firebasedatabase.app"
+});
