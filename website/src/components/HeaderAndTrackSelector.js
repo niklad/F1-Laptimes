7aafac873c8ccafcb83c21be638d2421c2c3b497
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-07-12 18:45:54 +0200

    Add most tracks

--- website/src/components/HeaderAndTrackSelector.js
+++ website/src/components/HeaderAndTrackSelector.js
@@ -7,12 +7,48 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
 const trackFlagMap = {
     "SILVERSTONE": "gb",
     "SPA": "be",
+    "SUZUKA": "jp",
+    "MONZA": "it",
+    "MONTREAL": "ca",
+    "MELBOURNE": "au",
+    "BAKU": "az",
+    "SINGAPORE": "sg",
+    "ABU DHABI": "ae",
+    "AUSTIN": "us",
+    "INTERLAGOS": "br",
+    "MONACO": "mc",
+    "BAHRAIN": "bh",
+    "SOCHI": "ru",
+    "HUNGARORING": "hu",
+    "BARCELONA": "es",
+    "ZANDVOORT": "nl",
+    "JEDDAH": "sa",
+    "IMOLA": "it",
+    "PAUL RICARD": "fr",
 };
 
 // A map of track names to track country
 const trackCountryMap = {
     "SILVERSTONE": "UNITED KINGDOM",
     "SPA": "BELGIUM",
+    "SUZUKA": "JAPAN",
+    "MONZA": "ITALY",
+    "MONTREAL": "CANADA",
+    "MELBOURNE": "AUSTRALIA",
+    "BAKU": "AZERBAIJAN",
+    "SINGAPORE": "SINGAPORE",
+    "ABU DHABI": "UNITED ARAB EMIRATES",
+    "AUSTIN": "UNITED STATES",
+    "INTERLAGOS": "BRAZIL",
+    "MONACO": "MONACO",
+    "BAHRAIN": "BAHRAIN",
+    "SOCHI": "RUSSIA",
+    "HUNGARORING": "HUNGARY",
+    "BARCELONA": "SPAIN",
+    "ZANDVOORT": "NETHERLANDS",
+    "JEDDAH": "SAUDI ARABIA",
+    "IMOLA": "ITALY",
+    "PAUL RICARD": "FRANCE",
 };
 
 
