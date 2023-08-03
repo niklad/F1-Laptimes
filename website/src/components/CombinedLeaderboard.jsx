033e2dcc6d1f285c687afad66520e93856617336
Niklas <niklast@stud.ntnu.no>
2023-08-03 14:07:40 +0200

    Copilot magic to make stuff async
    Fixes issue with driver standings not rendering on first try.

--- website/src/components/CombinedLeaderboard.jsx
+++ website/src/components/CombinedLeaderboard.jsx
@@ -1,6 +1,7 @@
 // import the CombinedLeaderboard.css styling from ../styles/CombinedLeaderboard.css
 import { findAndSortLaptimes } from "./Leaderboard";
 import { getDatabase, ref, onValue, update, get } from "firebase/database";
+import { useState, useEffect } from "react";
 
 import "../styles/Leaderboard.css";
 
@@ -12,53 +13,75 @@ import "../styles/Leaderboard.css";
 // Generally, (n-1) points are awarded to the driver with the nth quickest laptime.
 
 // Function to calculate the combined leaderboard points
-function calculateCombinedLeaderboardPoints(database, trackOptions) {
-    const combinedLeaderboardPoints = {};
-    for (const trackIndex in trackOptions) {
-        if (trackOptions[trackIndex] === "DRIVER STANDINGS") {
-            continue;
-        }
-        const trackRef = ref(database, trackOptions[trackIndex]);
-        onValue(trackRef, (snapshot) => {
-            const trackData = snapshot.val();
-            if (!trackData) {
-                return;
-            }
-            const latestLaptimes = findAndSortLaptimes(trackData);
-            for (let i = 0; i < latestLaptimes.length; i++) {
-                const driver = latestLaptimes[i].driver;
-                if (combinedLeaderboardPoints[driver] === undefined) {
-                    combinedLeaderboardPoints[driver] = 0;
-                }
-                combinedLeaderboardPoints[driver] +=
-                    latestLaptimes.length - i - 1;
-            }
-        });
-    }
-    return (
-        <tbody>
-            {Object.entries(combinedLeaderboardPoints)
-                .sort((a, b) => b[1] - a[1])
-                .map(([driver, points], index) => (
-                    <tr key={driver}>
-                        <td className="indexColumn">{index + 1}.</td>
-                        <td className="driverNameColumn">{driver}</td>
-                        <td className="driverStandingsPointsColumn">
-                            {points}
-                        </td>
-                    </tr>
-                ))}
-        </tbody>
-    );
-}
 
 export function CombinedLeaderboard({ database, trackOptions }) {
+    const [combinedLeaderboardPoints, setCombinedLeaderboardPoints] =
+        useState(null);
+
+    useEffect(() => {
+        async function fetchCombinedLeaderboardPoints() {
+            const promises = [];
+            const points = {};
+
+            for (const trackIndex in trackOptions) {
+                if (trackOptions[trackIndex] === "DRIVER STANDINGS") {
+                    continue;
+                }
+                const trackRef = ref(database, trackOptions[trackIndex]);
+                const promise = new Promise((resolve, reject) => {
+                    onValue(
+                        trackRef,
+                        (snapshot) => {
+                            const trackData = snapshot.val();
+                            if (!trackData) {
+                                resolve();
+                                return;
+                            }
+                            const latestLaptimes =
+                                findAndSortLaptimes(trackData);
+                            for (let i = 0; i < latestLaptimes.length; i++) {
+                                const driver = latestLaptimes[i].driver;
+                                if (points[driver] === undefined) {
+                                    points[driver] = 0;
+                                }
+                                points[driver] += latestLaptimes.length - i - 1;
+                            }
+                            resolve();
+                        },
+                        reject
+                    );
+                });
+                promises.push(promise);
+            }
+
+            await Promise.all(promises);
+
+            setCombinedLeaderboardPoints(points);
+        }
+
+        fetchCombinedLeaderboardPoints();
+    }, [database, trackOptions]);
+
+    if (!combinedLeaderboardPoints) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <>
-            <table className="leaderboard-table">
-                {calculateCombinedLeaderboardPoints(database, trackOptions)}
-            </table>
-        </>
+        <table className="leaderboard-table">
+            <tbody>
+                {Object.entries(combinedLeaderboardPoints)
+                    .sort((a, b) => b[1] - a[1])
+                    .map(([driver, points], index) => (
+                        <tr key={driver}>
+                            <td className="indexColumn">{index + 1}.</td>
+                            <td className="driverNameColumn">{driver}</td>
+                            <td className="driverStandingsPointsColumn">
+                                {points}
+                            </td>
+                        </tr>
+                    ))}
+            </tbody>
+        </table>
     );
 }
 
