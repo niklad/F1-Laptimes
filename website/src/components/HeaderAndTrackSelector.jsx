05160422bba5920abab8226952fc6bf95ebea57a
Niklas <niklast@stud.ntnu.no>
2023-08-03 11:03:58 +0200

    Won't set trophy in trackFlagMap

--- website/src/components/HeaderAndTrackSelector.jsx
+++ website/src/components/HeaderAndTrackSelector.jsx
@@ -4,7 +4,6 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 // A map of track names to flag icons
 export const trackFlagMap = {
-    "DRIVER STANDINGS": "no", // Replace with a trophy icon
     SILVERSTONE: "gb",
     SPA: "be",
     SUZUKA: "jp",
