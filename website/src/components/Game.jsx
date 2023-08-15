9ccbba8940cb53155e37109a4bbc00fafa7c87ae
Niklas <niklast@stud.ntnu.no>
2023-08-15 13:35:54 +0200

    Get the color of the pixel of the car location

--- website/src/components/Game.jsx
+++ website/src/components/Game.jsx
@@ -1,44 +1,66 @@
 import React, { useRef, useEffect } from "react";
-
 import "../styles/Game.css";
 
 export default function Game() {
     const canvasRef = useRef(null);
-    const backgroundImageRef = useRef(null);
     const backgroundXRef = useRef(0);
     const backgroundYRef = useRef(0);
-    const carImageRef = useRef(null);
     const keysRef = useRef({});
 
     useEffect(() => {
         const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
+        const context = canvas.getContext("2d");
 
         // Load the background image
         const backgroundImage = new Image();
         backgroundImage.src = "game/bilspill-demospor.png";
-        backgroundImageRef.current = backgroundImage;
+        backgroundImage.onload = () => {
+            canvas.width = 0.4 * window.innerWidth;
+            canvas.height = 0.9 * window.innerHeight;
+            // Set the initial position of the background image
+            // backgroundXRef.current = -375;
+            // backgroundYRef.current = -800;
+        };
 
         // Load the car image
         const carImage = new Image();
         carImage.src = "game/bilspill-bil-v1-transparent.png";
-        carImageRef.current = carImage;
-
-        // Draw the background image and car image on the canvas
-        backgroundImage.onload = () => {
-            canvas.width = backgroundImage.width;
-            canvas.height = backgroundImage.height;
-            ctx.drawImage(backgroundImage, 0, 0);
-
-            carImage.onload = () => {
-                const centerX = canvas.width / 2 - carImage.width / 2;
-                const centerY = canvas.height / 2 - carImage.height / 2;
-                ctx.drawImage(carImage, centerX, centerY);
-            };
+        carImage.onload = () => {
+            requestAnimationFrame(updateCanvas);
         };
-    }, []);
 
-    useEffect(() => {
+        const updateCanvas = () => {
+            if (keysRef.current["ArrowLeft"]) {
+                backgroundXRef.current += 5;
+            } else if (keysRef.current["ArrowRight"]) {
+                backgroundXRef.current -= 5;
+            }
+            if (keysRef.current["ArrowUp"]) {
+                backgroundYRef.current += 5;
+            } else if (keysRef.current["ArrowDown"]) {
+                backgroundYRef.current -= 5;
+            }
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(
+                backgroundImage,
+                backgroundXRef.current,
+                backgroundYRef.current
+            );
+
+            // Get the color of the pixel in the middle of the canvas
+            const cX = canvas.width / 2;
+            const cY = canvas.height / 2;
+            const imageData = context.getImageData(cX, cY, 1, 1);
+            let pixelColor = `rgb(${imageData.data[0]}, ${imageData.data[1]}, ${imageData.data[2]})`;
+            console.log(pixelColor);
+            const centerX = canvas.width / 2 - carImage.width / 2;
+            const centerY = canvas.height / 2 - carImage.height / 2;
+            context.drawImage(carImage, centerX, centerY);
+
+            requestAnimationFrame(updateCanvas);
+        };
+
         const handleKeyDown = (event) => {
             keysRef.current[event.key] = true;
         };
@@ -53,35 +75,10 @@ export default function Game() {
         };
     }, []);
 
-    useEffect(() => {
-        const updateCanvas = () => {
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d");
-            const backgroundImage = backgroundImageRef.current;
-            const carImage = carImageRef.current;
-            if (keysRef.current["ArrowLeft"]) {
-                backgroundXRef.current += 5;
-            } else if (keysRef.current["ArrowRight"]) {
-                backgroundXRef.current -= 5;
-            }
-            if (keysRef.current["ArrowUp"]) {
-                backgroundYRef.current += 5;
-            } else if (keysRef.current["ArrowDown"]) {
-                backgroundYRef.current -= 5;
-            }
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(
-                backgroundImage,
-                backgroundXRef.current,
-                backgroundYRef.current
-            );
-            const centerX = canvas.width / 2 - carImage.width / 2;
-            const centerY = canvas.height / 2 - carImage.height / 2;
-            ctx.drawImage(carImage, centerX, centerY);
-            requestAnimationFrame(updateCanvas);
-        };
-        requestAnimationFrame(updateCanvas);
-    }, []);
-
-    return <canvas ref={canvasRef} id="game" />;
+    return (
+        <div>
+            <canvas ref={canvasRef} id="game" />
+            <p>hello</p>
+        </div>
+    );
 }
