9e1c8334c9eb58986d2f3dd5ed1cdd99fd3ce14c
Niklas <niklast@stud.ntnu.no>
2023-08-16 01:34:04 +0200

    Actually fix acceleration
    Lønner seg å tenke litt

--- website/src/components/Game.jsx
+++ website/src/components/Game.jsx
@@ -8,20 +8,15 @@ export default function Game() {
     const backgroundYRef = useRef(startCoordinates.y);
     const backgroundRotationRef = useRef(0);
     const keysRef = useRef({});
-    let centerPixelColor = useRef("rgb(41, 41, 41)");
+    let centerPixelColorRef = useRef("rgb(41, 41, 41)");
+    let velocityRef = useRef({ vx: 0, vy: 0 });
 
     // Define the acceleration and deceleration constants
     const THROTTLE_ACCELERATION = 0.05;
     const BRAKE_DECELERATION = 0.05;
-    const GEAR_BRAKE_DECELERATION = 0.1;
-    const MIN_VELOCITY = 0;
-    const MAX_VELOCITY = 5;
+    const GEAR_BRAKE_DECELERATION = 0.02;
+    const MAX_VELOCITY = 6;
     const STEERING_ACCELERATION = 0.05;
-
-    // Define the current velocity of the car
-    let velocityX = 0;
-    let velocityY = 0;
-    let steeringAngle = 0;
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -43,9 +38,8 @@ export default function Game() {
         };
 
         const updateCanvas = () => {
-            if (centerPixelColor !== "rgb(41, 41, 41)") {
-                velocityX = 0;
-                velocityY = 0;
+            if (centerPixelColorRef !== "rgb(41, 41, 41)") {
+                velocityRef.current = { vx: 0, vy: 0 };
                 setTimeout(() => {
                     // reset the car position after 300ms
                     backgroundXRef.current = startCoordinates.x;
@@ -55,65 +49,18 @@ export default function Game() {
             }
 
             if (keysRef.current["ArrowLeft"]) {
-                backgroundRotationRef.current += 0.015;
-                velocityX += Math.sin(backgroundRotationRef.current);
-                velocityY += Math.cos(backgroundRotationRef.current);
-                // 0.01 *
-                // Math.log(Math.sqrt(velocityX ** 2 + velocityY ** 2));
-                // velocityY -= Math.cos(backgroundRotationRef.current) * 0.1;
-            } else if (keysRef.current["ArrowRight"]) {
                 backgroundRotationRef.current -= 0.015;
-                velocityX += Math.sin(backgroundRotationRef.current);
-                velocityY += Math.cos(backgroundRotationRef.current);
-                // 0.01 *
-                // Math.log(Math.sqrt(velocityX ** 2 + velocityY ** 2));
-
-                // velocityY += Math.cos(backgroundRotationRef.current) * 0.1;
+            } else if (keysRef.current["ArrowRight"]) {
+                backgroundRotationRef.current += 0.015;
             }
-            // Convert the velocity vectors based on the reference point of the car instead of the background
-            console.log(
-                "velocityX:",
-                velocityX,
-                "velocityY:",
-                velocityY,
-                "backgroundRotationRef.current:",
-                backgroundRotationRef.current,
-                "sin(backgroundRotationRef.current):",
-                Math.sin(backgroundRotationRef.current),
-                "cos(backgroundRotationRef.current):",
-                Math.cos(backgroundRotationRef.current)
-            );
+
             if (keysRef.current["ArrowUp"]) {
-                velocityX +=
-                    Math.sin(backgroundRotationRef.current) *
-                    THROTTLE_ACCELERATION;
-                velocityY +=
-                    Math.cos(backgroundRotationRef.current) *
-                    THROTTLE_ACCELERATION;
+                velocityRef.current.vy += THROTTLE_ACCELERATION;
+                if (velocityRef.current.vy >= MAX_VELOCITY) {
+                    velocityRef.current.vy = MAX_VELOCITY;
+                }
             } else {
-                // Deal with the deceleration when the background image is oriented in any direction
-                // if (velocityX > 0) {
-                //     velocityX -=
-                //         Math.sin(backgroundRotationRef.current) *
-                //         GEAR_BRAKE_DECELERATION;
-                // }
-                // if (velocityX < 0) {
-                //     velocityX +=
-                //         Math.sin(backgroundRotationRef.current) *
-                //         GEAR_BRAKE_DECELERATION;
-                // }
-                // if (velocityY > 0) {
-                //     velocityY -=
-                //         Math.cos(backgroundRotationRef.current) *
-                //         GEAR_BRAKE_DECELERATION;
-                // }
-                // if (velocityY < 0) {
-                //     velocityY +=
-                //         Math.cos(backgroundRotationRef.current) *
-                //         GEAR_BRAKE_DECELERATION;
-                // }
-                velocityX = 0;
-                velocityY = 0;
+                velocityRef.current.vy = 0;
             }
 
             // Clear the canvas
@@ -123,21 +70,24 @@ export default function Game() {
             context.save();
             // Rotate around the center of the canvas
             context.translate(canvas.width / 2, canvas.height / 2);
-            context.rotate(backgroundRotationRef.current);
+            context.rotate(-backgroundRotationRef.current);
             context.translate(-canvas.width / 2, -canvas.height / 2);
 
-            // Limit the velocity to the maximum value
-            const currentVelocity = Math.sqrt(velocityX ** 2 + velocityY ** 2);
-            if (currentVelocity > MAX_VELOCITY) {
-                const angle = Math.atan2(velocityY, velocityX);
-                velocityX = Math.cos(angle) * MAX_VELOCITY;
-                velocityY = Math.sin(angle) * MAX_VELOCITY;
-            }
+            backgroundXRef.current +=
+                Math.cos(backgroundRotationRef.current) *
+                    velocityRef.current.vx -
+                Math.sin(backgroundRotationRef.current) *
+                    velocityRef.current.vy;
+            backgroundYRef.current +=
+                Math.sin(backgroundRotationRef.current) *
+                    velocityRef.current.vx +
+                Math.cos(backgroundRotationRef.current) *
+                    velocityRef.current.vy;
+
             context.drawImage(
                 backgroundImage,
-                // Update the position based on the velocity
-                (backgroundXRef.current += velocityX),
-                (backgroundYRef.current += velocityY)
+                backgroundXRef.current,
+                backgroundYRef.current
             );
 
             context.restore();
@@ -151,7 +101,8 @@ export default function Game() {
                 1,
                 1
             );
-            centerPixelColor = `rgb(${imageData.data[0]}, ${imageData.data[1]}, ${imageData.data[2]})`;
+            centerPixelColorRef = `rgb(${imageData.data[0]}, ${imageData.data[1]}, ${imageData.data[2]})`;
+
             const centerX = canvas.width / 2 - carImage.width / 2;
             const centerY = canvas.height / 2 - carImage.height / 2;
             context.drawImage(carImage, centerX, centerY);
