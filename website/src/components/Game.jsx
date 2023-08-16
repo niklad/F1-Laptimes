b969ca86d623350b0e8db6aca6ca4b72b1041874
Niklas <niklast@stud.ntnu.no>
2023-08-16 12:07:18 +0200

    Buggy timer and some drifting sims

--- website/src/components/Game.jsx
+++ website/src/components/Game.jsx
@@ -3,20 +3,23 @@ import "../styles/Game.css";
 
 export default function Game() {
     const canvasRef = useRef(null);
-    const startCoordinates = { x: -370, y: -1000 };
+    const startCoordinates = { x: -560, y: -1300 };
     const backgroundXRef = useRef(startCoordinates.x);
     const backgroundYRef = useRef(startCoordinates.y);
     const backgroundRotationRef = useRef(0);
     const keysRef = useRef({});
+    const timerRef = useRef(0);
+    const bestLaptimeRef = useRef(0.0);
     let centerPixelColorRef = useRef("rgb(41, 41, 41)");
     let velocityRef = useRef({ vx: 0, vy: 0 });
 
     // Define the acceleration and deceleration constants
     const THROTTLE_ACCELERATION = 0.05;
-    const BRAKE_DECELERATION = 0.05;
-    const GEAR_BRAKE_DECELERATION = 0.02;
-    const MAX_VELOCITY = 6;
-    const STEERING_ACCELERATION = 0.05;
+    const BRAKE_DECELERATION = 0.1;
+    const REVERSE_ACCELERATION = 0.025;
+    const GEAR_BRAKE_DECELERATION = 0.05;
+    const MAX_VELOCITY = 9;
+    const STEERING_ACCELERATION = 0.015;
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -37,21 +40,47 @@ export default function Game() {
             requestAnimationFrame(updateCanvas);
         };
 
-        const updateCanvas = () => {
+        const updateCanvas = (timestamp) => {
+            if (timerRef.current === 0) {
+                timerRef.current = timestamp;
+            }
+            const seconds = Math.floor((timestamp - timerRef.current) / 1000);
+            const milliseconds = timestamp - timerRef.current - seconds * 1000;
+
+            if (centerPixelColorRef === "rgb(134, 2, 0)") {
+                // Update bestLaptime if the current laptime on the format is better
+                const currentLaptime = seconds + milliseconds / 1000;
+                if (currentLaptime < bestLaptimeRef.current) {
+                    bestLaptimeRef.current = currentLaptime;
+                }
+            }
             if (centerPixelColorRef !== "rgb(41, 41, 41)") {
-                velocityRef.current = { vx: 0, vy: 0 };
+                timerRef.current = 0;
                 setTimeout(() => {
-                    // reset the car position after 300ms
+                    velocityRef.current.vy = 0;
+                    velocityRef.current.vx = 0;
+                    backgroundRotationRef.current = 0;
                     backgroundXRef.current = startCoordinates.x;
                     backgroundYRef.current = startCoordinates.y;
-                    backgroundRotationRef.current = 0;
                 }, 600);
             }
 
-            if (keysRef.current["ArrowLeft"]) {
-                backgroundRotationRef.current -= 0.015;
+            if (velocityRef.current.vy === 0) {
+                backgroundRotationRef.current = 0;
+            } else if (keysRef.current["ArrowLeft"]) {
+                backgroundRotationRef.current -= STEERING_ACCELERATION;
+                velocityRef.current.vx -=
+                    0.004 * (MAX_VELOCITY - velocityRef.current.vy);
             } else if (keysRef.current["ArrowRight"]) {
-                backgroundRotationRef.current += 0.015;
+                backgroundRotationRef.current += STEERING_ACCELERATION;
+                velocityRef.current.vx +=
+                    0.006 * (MAX_VELOCITY - velocityRef.current.vy);
+            } else {
+                if (velocityRef.current.vx > 0) {
+                    velocityRef.current.vx -= BRAKE_DECELERATION;
+                } else if (velocityRef.current.vx < 0) {
+                    velocityRef.current.vx += BRAKE_DECELERATION;
+                }
             }
 
             if (keysRef.current["ArrowUp"]) {
@@ -59,8 +88,20 @@ export default function Game() {
                 if (velocityRef.current.vy >= MAX_VELOCITY) {
                     velocityRef.current.vy = MAX_VELOCITY;
                 }
+            } else if (keysRef.current["ArrowDown"]) {
+                if (velocityRef.current.vy > 0) {
+                    velocityRef.current.vy -= BRAKE_DECELERATION;
+                } else if (velocityRef.current.vy < 0) {
+                    velocityRef.current.vy -= REVERSE_ACCELERATION;
+                } else {
+                    velocityRef.current.vy = 0;
+                }
             } else {
-                velocityRef.current.vy = 0;
+                if (velocityRef.current.vy > 0) {
+                    velocityRef.current.vy -= GEAR_BRAKE_DECELERATION;
+                } else if (velocityRef.current.vy < 0) {
+                    velocityRef.current.vy += GEAR_BRAKE_DECELERATION;
+                }
             }
 
             // Clear the canvas
@@ -72,6 +113,10 @@ export default function Game() {
             context.translate(canvas.width / 2, canvas.height / 2);
             context.rotate(-backgroundRotationRef.current);
             context.translate(-canvas.width / 2, -canvas.height / 2);
+
+            // if (velocityRef.current.vy < 0.01) {
+            //     velocityRef.current.vy = 0;
+            // }
 
             backgroundXRef.current +=
                 Math.cos(backgroundRotationRef.current) *
@@ -106,6 +151,22 @@ export default function Game() {
             const centerX = canvas.width / 2 - carImage.width / 2;
             const centerY = canvas.height / 2 - carImage.height / 2;
             context.drawImage(carImage, centerX, centerY);
+
+            // Draw the timer
+            context.fillStyle = "white";
+            context.font = "24px Arial";
+            context.fillText(
+                `${(seconds + milliseconds / 1000).toFixed(3)}`,
+                10,
+                30
+            );
+
+            // Draw best laptime
+            context.fillText(
+                `Best laptime: ${bestLaptimeRef.current.toFixed(3)}`,
+                10,
+                60
+            );
 
             requestAnimationFrame(updateCanvas);
         };
