11c6d5b56634ea143f2612f458efb36433362928
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-08-16 16:40:27 +0200

    Fix arrows changing track when in an input field

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -177,6 +177,9 @@ function useArrowsForNavigation(track, setTrack) {
         const handleKeyDown = (event) => {
             let currentIndex;
             let newIndex;
+            if (document.activeElement.tagName === "INPUT") {
+                return;
+            }
             switch (event.key) {
                 case "ArrowUp":
                     currentIndex = trackOptions.indexOf(track);
